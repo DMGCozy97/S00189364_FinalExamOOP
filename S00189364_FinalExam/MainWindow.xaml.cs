8237c303c7c1468460dfe173ebf7d8a96e397707
s00189364 <s00189364@LAB-C1002-12>
2019-12-11 10:28:04 +0000

    stage 1

--- S00189364_FinalExam/MainWindow.xaml.cs
+++ S00189364_FinalExam/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,70 @@ namespace S00189364_FinalExam
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Player> allPlayers = new List<Player>();
+        List<Player> selectedPlayers = new List<Player>();
+        Random rng = new Random();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void AllPlayersLbx_Loaded(object sender, RoutedEventArgs e)
+        {
+            //create list items
+            Player firstplayer = new Player(GetRandomPlayer());
+            Player secondplayer = new Player(GetRandomPlayer());
+            Player thirdplayer = new Player(GetRandomPlayer());
+            Player fourthplayer = new Player(GetRandomPlayer());
+            
+            //addtolist
+            allPlayers.Add(firstplayer);
+            allPlayers.Add(secondplayer);
+            allPlayers.Add(thirdplayer);
+            allPlayers.Add(fourthplayer);
+
+
+            allPlayersLbx.ItemsSource = allPlayers;
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //Randomiser Method
+        private Player GetRandomPlayer()
+        {
+            //get rndom FirstName
+            string[] randomfirstnames = { "John, Ryan, Dylan, Michael" };
+            int randomNumber = rng.Next(0, 4);
+            string firstname = randomfirstnames[randomNumber];
+            //get rndom SurName
+            string[] randomsurnames = { "McCarthy, Fitzpatrick, Cooney, Walsh" };
+            int randomNumber2 = rng.Next(0, 4);
+            string surname = randomsurnames[randomNumber];
+            //get rndom postion
+            string[] randompositions = { "Goalkeeper, Defender, Midfielder, Forward" };
+            int randomNumber3 = rng.Next(0, 4);
+            string preferedPosition = randompositions[randomNumber];
+            //get random age
+            //get rndom FirstName
+            int[] randomages = { 20, 25, 28, 30 };
+            int randomNumber4 = rng.Next(0, 4);
+            int age = randomages[randomNumber];
+            //get rndom datetime
+            randomNumber = rng.Next(0, 31);
+            DateTime randomDate = DateTime.Now.AddDays(-randomNumber);
+            //create expense object with randominfo 
+            Player p1 = new Player(firstname, surname, preferedPosition, randomDate, age);
+
+            //return 
+            return p1;
+
+        }
+
+
     }
 }
---
+++ S00189364_FinalExam/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00189364_FinalExam
+{
+
+    //creating enum
+    public enum Position { Goalkeeper, Defender, Midfielder, Forward }
+    class Player : IComparable<Player>
+    {
+        //Proporties
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+
+        public Position PreferedPosition { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        private int _age;
+        private string preferedPosition;
+        private DateTime randomDate;
+        private Player player;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        //Constructors
+
+        //public Player(string firstName, string surname, Position preferedPosition, DateTime dateOfBirth, int age)
+        //{
+        //    this.FirstName = firstName;
+        //    this.Surname = surname;
+        //    this.PreferedPosition = preferedPosition;
+        //    this.DateOfBirth = dateOfBirth;
+        //    this.Age = age;
+        //}
+
+        public Player(string firstname, string surname, string preferedPosition, DateTime randomDate, int age)
+        {
+            FirstName = firstname;
+            Surname = surname;
+            this.preferedPosition = preferedPosition;
+            this.randomDate = randomDate;
+            Age = age;
+        }
+
+        public Player(Player player)
+        {
+            this.player = player;
+        }
+
+        //METHODS
+        public override string ToString()
+        {
+            return $"{FirstName} {Surname} {PreferedPosition} on {DateOfBirth.ToShortDateString()}";
+        }
+
+        public int CompareTo(Player other)
+        {
+            return this.PreferedPosition.CompareTo(other.PreferedPosition);
+        }
+
+
+    }
+}
